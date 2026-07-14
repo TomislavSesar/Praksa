7ff4588ec993ab047ce54caaf832d4095a4353ed
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2026-07-14 14:56:23 +0200

    xdp: Add UDP destination port filtering
    Without port filtering, XDP program forwards all incoming UDP
    traffic to AF_XDP socket regardless of destination port.
    This prevents selective traffic processing and wastes resources
    on unwanted packets.
    
    Added headers that help us in packet header parsing. Drop packets
    whose destination port falls outside the
    configured range, and redirect the rest to AF_XDP socket
    via xsks_map. For more details
    Praksa/AF_XDP/my_xdp_prog.c, xdp_prog function.

--- AF_XDP/my_xdp_prog.c
+++ AF_XDP/my_xdp_prog.c
@@ -1,6 +1,10 @@
-#include <linux/bpf.h>
+#include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
-
+#include <linux/bpf.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 struct {
     __uint(type, BPF_MAP_TYPE_XSKMAP);
@@ -11,9 +15,42 @@ struct {
 
 SEC("xdp")
 
-int xdp_prog(struct xdp_md *ctx) {
-    
-    bpf_printk("%d. frame recvied");
+int xdp_prog(struct xdp_md *ctx)
+{
+
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    struct ether_header *eth = data;
+
+    if ((void *)(eth + 1) > data_end) {
+        return XDP_PASS;
+    }
+
+    struct iphdr *iph =
+        (struct iphdr *)((uint8_t *)eth + sizeof(struct ether_header));
+
+    if ((void *)(iph + 1) > data_end) {
+        return XDP_PASS;
+    }
+
+    if (iph->protocol == IPPROTO_UDP) {
+        struct udphdr *udph = data + sizeof(struct ether_header) + iph->ihl * 4;
+        if ((void *)(udph + 1) > data_end)
+            return XDP_PASS;
+
+        __u16 dport = bpf_ntohs(udph->dest);
+
+        if (dport < 10 || dport > 100) {
+            bpf_printk("DROP port=%d", udph->dest);
+            bpf_printk("DROP port_nthos=%d", ntohs(udph->dest));
+            return XDP_DROP;
+        }
+        bpf_printk("PASS port=%d", udph->dest);
+        bpf_printk("PASS port_nthos=%d", ntohs(udph->dest));
+    }
+
+    bpf_printk("frame recvied");
 
     return bpf_redirect_map(&xsks_map, ctx->rx_queue_index, XDP_PASS);
 }
