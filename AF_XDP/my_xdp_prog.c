ce0a9c4f7416a400df3e84b7261bdd348656d7dd
TomislavSesar <tomislav.sesar@fer.unizg.hr>
2026-07-15 10:47:29 +0200

    xdp: Fix build failure due to userspace headers
    XDP programs run in kernel space where only kernel headers from
    /usr/include/linux/ and /usr/include/bpf/ are permitted. The
    program included userspace headers which are not available in the
    BPF verifier context, causing a build failure:
    
    /usr/include/bpf/bpf_helper_defs.h:78:83: error: unknown type name '__u64'
    /usr/include/bpf/bpf_helper_defs.h:102:42: error: unknown type name '__u32'
    
    Replace userspace headers with their kernel-space equivalents.
    Update struct ethhdr usage to conform to the kernel header
    definition, as the field layout differs between userspace and
    kernel variants.

--- AF_XDP/my_xdp_prog.c
+++ AF_XDP/my_xdp_prog.c
@@ -1,10 +1,12 @@
-#include <bpf/bpf_endian.h>
-#include <bpf/bpf_helpers.h>
+#include <linux/types.h>
 #include <linux/bpf.h>
-#include <netinet/ether.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <linux/in.h> 
 
 struct {
     __uint(type, BPF_MAP_TYPE_XSKMAP);
@@ -14,44 +16,34 @@ struct {
 } xsks_map SEC(".maps");
 
 SEC("xdp")
-
 int xdp_prog(struct xdp_md *ctx)
 {
-
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
-    struct ether_header *eth = data;
-
-    if ((void *)(eth + 1) > data_end) {
+    struct ethhdr *eth = data;                   
+    if ((void *)(eth + 1) > data_end)
         return XDP_PASS;
-    }
 
-    struct iphdr *iph =
-        (struct iphdr *)((uint8_t *)eth + sizeof(struct ether_header));
-
-    if ((void *)(iph + 1) > data_end) {
+    struct iphdr *iph = (struct iphdr *)((__u8 *)eth + sizeof(struct ethhdr));
+    if ((void *)(iph + 1) > data_end)
         return XDP_PASS;
-    }
 
     if (iph->protocol == IPPROTO_UDP) {
-        struct udphdr *udph = data + sizeof(struct ether_header) + iph->ihl * 4;
+        struct udphdr *udph = data + sizeof(struct ethhdr) + iph->ihl * 4;
         if ((void *)(udph + 1) > data_end)
             return XDP_PASS;
 
         __u16 dport = bpf_ntohs(udph->dest);
 
         if (dport < 10 || dport > 100) {
-            bpf_printk("DROP port=%d", udph->dest);
-            bpf_printk("DROP port_nthos=%d", ntohs(udph->dest));
+            bpf_printk("DROP port=%d", dport);
             return XDP_DROP;
         }
-        bpf_printk("PASS port=%d", udph->dest);
-        bpf_printk("PASS port_nthos=%d", ntohs(udph->dest));
+        bpf_printk("PASS port=%d", dport);
     }
 
-    bpf_printk("frame recvied");
-
+    bpf_printk("frame received");
     return bpf_redirect_map(&xsks_map, ctx->rx_queue_index, XDP_PASS);
 }
 
